8a5d9081f55060d3aeb6b0037c1889b923fba346
Sameh Refaay <samehrefaay89@gmail.com>
2023-03-30 00:54:41 +0200

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import "./App.css";
 
 const App = () => {
   let [todos, setTodos] = useState([]);
+  const [todoToShow, setTodoToShow] = useState("all");
+  const [toggleAllComplete, setToggleAllComplete] = useState(true);
   const addTodo = (todo) => {
     setTodos([todo, ...todos]);
   };
@@ -22,6 +24,21 @@ const App = () => {
       })
     );
   };
+  const handleTodoToShow = (s) => {
+    setTodoToShow(s);
+  };
+  if (todoToShow === "active") {
+    todos = todos.filter((todo) => !todo.isComplete);
+  } else if (todoToShow === "complete") {
+    todos = todos.filter((todo) => todo.isComplete);
+  }
+  const handleRemoveAllComplete = () => {
+    setTodos(todos.filter((todo) => !todo.isComplete));
+  };
+  const handleToggleAllComplete = () => {
+    setToggleAllComplete(!toggleAllComplete);
+    setTodos(todos.map((todo) => ({ ...todo, isComplete: toggleAllComplete })));
+  };
   return (
     <div>
       <div className="main-container">
@@ -34,6 +51,39 @@ const App = () => {
             toggleComplete={() => handleToggleComplete(todo.id)}
           />
         ))}
+        <div className="btns-container">
+          <button
+            className="update-btn btn"
+            onClick={() => handleTodoToShow("all")}
+          >
+            All
+          </button>
+          <button
+            className="update-btn btn"
+            onClick={() => handleTodoToShow("active")}
+          >
+            Active
+          </button>
+          <button
+            className="update-btn btn"
+            onClick={() => handleTodoToShow("complete")}
+          >
+            Complete
+          </button>
+        </div>
+        <div className="btns-container">
+          {todos.some((todo) => todo.isComplete) ? (
+            <button
+              className="update-btn btn"
+              onClick={handleRemoveAllComplete}
+            >
+              Remove All Complete Todos
+            </button>
+          ) : null}
+          <button className="update-btn btn" onClick={handleToggleAllComplete}>
+            Toggle All Complete: {`${toggleAllComplete}`}
+          </button>
+        </div>
       </div>
     </div>
   );
